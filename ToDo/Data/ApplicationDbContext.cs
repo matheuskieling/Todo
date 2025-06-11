f50d40f2964b6fc45005cd9ad817fb40fb593021
Matheus Kieling <matheus.kieling@serendipe.org>
2025-06-11 16:42:18 -0300

    add database integration

---
+++ ToDo/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.Models.Entities;
+
+namespace ToDo.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Todo> Todos { get; set; }
+    public DbSet<TodoCategory> TodoCategories { get; set; }
+}
