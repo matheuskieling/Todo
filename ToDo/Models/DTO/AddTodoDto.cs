fa33b2441aed1c39d61f766493eeb83d563c1b3f
Matheus Kieling <matheus.kieling@serendipe.org>
2025-06-11 16:49:03 -0300

    Adding Todo Entity

---
+++ ToDo/Models/DTO/AddTodoDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ToDo.Models.Entities;
+
+namespace ToDo.Models.DTO;
+
+public record AddTodoDto(
+    [Required] string Title,
+    string? Description,
+    TodoCategory? Category
+);
---
+++ ToDo/Models/Entities/Todo.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDo.Models.Entities;
+
+public class Todo
+{
+    [Key]
+    public Guid Id { get; set; }
+    
+    public required string Title { get; set; } = null!;
+    public string? Description { get; set; } = null!;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; } = null;
+    public DateTime? CompletedAt { get; set; } = null;
+    public TodoCategory? Category { get; set; } = null;
+}
