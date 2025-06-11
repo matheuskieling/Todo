65382c5bbf3974e9b3575ed05e5cfb6aa132c7b8
Matheus Kieling <matheus.kieling@serendipe.org>
2025-06-11 16:43:33 -0300

    adding dependencyInjection

---
+++ ToDo/Configuration/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.Data;
+
+namespace ToDo.Configuration;
+
+public static class DependencyInjection
+{
+    public static void DependencyInjectionInit(this IServiceCollection services, IConfiguration configuration)
+    {
+        #region DB
+        services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(
+            configuration.GetConnectionString("DefaultConnection")));
+        #endregion
+        
+        // #region Services
+        // #endregion
+        
+        // #region Repositories
+        // #endregion
+        
+        
+    }
+}
