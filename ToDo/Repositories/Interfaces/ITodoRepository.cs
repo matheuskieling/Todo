dffd1d97a4f26070a847a0a3aded447bdb49311b
Matheus Kieling <matheus.kieling@serendipe.org>
2025-06-11 18:05:39 -0300

    making repo

---
+++ ToDo/Repositories/Interfaces/ITodoRepository.cs
@@ -0,0 +1,14 @@
+﻿using ToDo.Models.DTO;
+using ToDo.Models.Entities;
+
+namespace ToDo.Repositories.Interfaces;
+
+public interface ITodoRepository
+{
+    Task<List<Todo>> GetAllAsync();
+    Task<Todo?> GetByIdAsync(Guid id);
+    Task<Todo> AddTodoAsync(AddTodoDto dto);
+    Task<bool> CompleteAsync(Guid id);
+    Task<Todo?> UpdateTodoAsync(Guid id, UpdateTodoDto dto);
+    Task<bool> DeleteAsync(Guid id);
+}
---
+++ ToDo/Repositories/TodoRepository.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.Data;
+using ToDo.Models.DTO;
+using ToDo.Models.Entities;
+using ToDo.Repositories.Interfaces;
+
+namespace ToDo.Repositories;
+
+public class TodoRepository(ApplicationDbContext context) : ITodoRepository
+{
+    public async Task<List<Todo>> GetAllAsync()
+    {
+        return await context.Todos.ToListAsync();
+    }
+
+    public async Task<Todo?> GetByIdAsync(Guid id)
+    {
+        return await context.Todos.FindAsync(id);
+    }
+
+    public async Task<Todo> AddTodoAsync(AddTodoDto dto)
+    {
+        var category = await context.TodoCategories.FirstOrDefaultAsync(ct => ct.Name == dto.CategoryName);
+        var todo = new Todo()
+        {
+            Title = dto.Title,
+            Description = dto.Description,
+            Category = category,
+        };
+        await context.Todos.AddAsync(todo);
+        await context.SaveChangesAsync();
+        return todo;
+    }
+
+    public async Task<bool> CompleteAsync(Guid id)
+    {
+        var todo = await context.Todos.FindAsync(id);
+        if (todo is null)
+        {
+            return false;
+        }
+        
+        todo.UpdatedAt = DateTime.UtcNow;
+        todo.CompletedAt = DateTime.UtcNow;
+        todo.IsDone = true;
+
+        context.Todos.Update(todo);
+        await context.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<Todo?> UpdateTodoAsync(Guid id, UpdateTodoDto dto)
+    {
+        var todo = await context.Todos.FindAsync(id);
+        var category = await context.TodoCategories.FirstOrDefaultAsync(ct => ct.Name == dto.CategoryName);
+        if (todo is null)
+        {
+            return null;
+        }
+        todo.Title = dto.Title ?? todo.Title;
+        todo.Description = dto.Description ?? todo.Description;
+        todo.Category = category ?? todo.Category;
+        todo.UpdatedAt = DateTime.UtcNow;
+        
+        context.Todos.Update(todo);
+        await context.SaveChangesAsync();
+        
+        return todo;
+    }
+
+    public async Task<bool> DeleteAsync(Guid id)
+    {
+        var todo = await context.Todos.FindAsync(id);
+        if (todo is null)
+        {
+            return false;
+        }
+        context.Todos.Remove(todo);
+        await context.SaveChangesAsync();
+        
+        return true;
+    }
+}
