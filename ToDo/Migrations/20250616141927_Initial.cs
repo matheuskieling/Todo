6a987213336bbdcb8cb0fdd6ca59f01ac7d122c5
Matheus Kieling <matheus.kieling@hotmail.com>
2025-06-16 11:23:11 -0300

    migration

---
+++ ToDo/Migrations/20250616141927_Initial.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ToDo.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "todo");
+
+            migrationBuilder.EnsureSchema(
+                name: "identity");
+
+            migrationBuilder.CreateTable(
+                name: "TodoCategories",
+                schema: "todo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                schema: "identity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    PasswordSalt = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Todos",
+                schema: "todo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    IsDone = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    CompletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    CategoryId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Todos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Todos_TodoCategories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalSchema: "todo",
+                        principalTable: "TodoCategories",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.InsertData(
+                schema: "todo",
+                table: "TodoCategories",
+                columns: new[] { "Id", "CreatedAt", "Name", "UpdatedAt", "UserId" },
+                values: new object[,]
+                {
+                    { new Guid("08237ce3-24c8-4504-8548-e1661abadcd4"), new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), "Work", new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), new Guid("573440b8-55e8-4f69-a7fc-e693718c9b45") },
+                    { new Guid("4e3b1dcf-8010-47c2-a712-45bf8cce1877"), new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), "Others", new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), new Guid("573440b8-55e8-4f69-a7fc-e693718c9b45") },
+                    { new Guid("710a4d1f-d814-4311-bdb6-ee60c69d937d"), new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), "Pets", new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), new Guid("573440b8-55e8-4f69-a7fc-e693718c9b45") },
+                    { new Guid("75cd5777-ecfc-4012-af5d-2f2c4491220e"), new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), "House", new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), new Guid("573440b8-55e8-4f69-a7fc-e693718c9b45") }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Todos_CategoryId",
+                schema: "todo",
+                table: "Todos",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Todos_Title",
+                schema: "todo",
+                table: "Todos",
+                column: "Title",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Username",
+                schema: "identity",
+                table: "Users",
+                column: "Username",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Todos",
+                schema: "todo");
+
+            migrationBuilder.DropTable(
+                name: "Users",
+                schema: "identity");
+
+            migrationBuilder.DropTable(
+                name: "TodoCategories",
+                schema: "todo");
+        }
+    }
+}
